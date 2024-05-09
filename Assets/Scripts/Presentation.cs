e790ac9c7fb9a610885c12fdc4a347f70f3155ba
Marco <marco.lavacchielli@davinci.edu.ar>
2024-05-09 17:42:56 -0300

    Tupla y Zip

---
+++ Assets/Scripts/Presentation.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using TMPro;
+using UnityEngine;
+
+public class Presentation : MonoBehaviour
+{
+    public TMP_Text textUI;
+
+    void Start()
+    {
+        Students();
+    }
+
+    void Students()
+    {
+        var names = new List<string> { "Marco", "Santiago", "Facundo" };
+        var lastNames = new List<string> { "Lavacchielli", "Sanchez", "Tisera" };
+
+        var studentData = names.Zip(lastNames, (name, lastName) => (Name: name, LastName: lastName));
+
+        string finalText = "Names of the students:\n";
+        foreach (var student in studentData)
+        {
+            finalText += $"{student.Name} {student.LastName}\n";
+        }
+
+        textUI.text = finalText;
+    }
+}
