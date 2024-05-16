e4f52e56dd51db54d8d1f50cfa619f9c8086e27a
facutise <facundo.tisera123@gmail.com>
2024-05-16 16:15:46 -0300

    nuevo aggregate con tupla

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,22 +3,68 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
 using UnityEngine.TextCore.Text;
+using System;
 
 public class GameManager : MonoBehaviour
 {
     public Characters[] Characters;
-    public List<Character> CharacterList;
+   
     private void Awake()
     {
         Characters = FindObjectsOfType<Characters>();
     }
     private void Start()
     {
-        forEachMac(mac(DeterminateRouteOfCharacters()));
-        forEachBank(Bank(DeterminateRouteOfCharacters()));
-        //Characters= FindObjectsOfType<Characters>();
-        //Characters =DeterminateRouteOfCharacters();
+        //forEachMac(mac(DeterminateRouteOfCharacters()));
+        //forEachBank(Bank(DeterminateRouteOfCharacters()));
+        
+        ToBankOrMacDefinitive(DeterminateRouteOfCharacters());
 
+    }
+    public void ToBankOrMacDefinitive(List<Characters> FirstFilter)
+    {
+        var grupos = FirstFilter.Aggregate(
+            Tuple.Create(new List<Characters>(), new List<Characters>()),
+            (acumulado, personaje) =>
+            {
+                
+                if (personaje.iHaveMoney==0)
+                {
+                    acumulado.Item1.Add(personaje); 
+                }
+                else
+                {
+                    acumulado.Item2.Add(personaje); 
+                }
+                return acumulado;
+            });
+        foreach (var personaje in grupos.Item1)
+        {
+            personaje.BankRoute();
+        }
+        foreach (var personaje in grupos.Item2)
+        {
+            personaje.MCDonaldsRoute();
+        }
+    }
+
+
+    public List<Characters> DeterminateRouteOfCharacters()
+    {
+        //Characters[] charactersInScene = FindObjectsOfType<Characters>();
+
+        List<Characters> ineligibleCharacters = Characters.Aggregate(new List<Characters>(), (list, character) =>
+        {
+            if (character.WantsToAttendConcert && (character.iHaveMoney <= 10 || character.Age < 18))
+            {
+                list.Add(character);
+            }
+            return list;
+        });
+        
+        Debug.Log("Personajes inelegibles para asistir al recital: " + ineligibleCharacters.Count);
+        
+        return ineligibleCharacters;
     }
     /*1 primero hago un get component en el start para que tenga a todos los characters
      * 2 ejecuto una funcion con Linq que devuelva una lista con los que van al mac
@@ -26,6 +72,7 @@ public class GameManager : MonoBehaviour
      
      
      */
+    /*
     public void forEachMac(List<Characters> ineligibleCharacters)
     {
         foreach(Characters character in ineligibleCharacters)
@@ -101,34 +148,8 @@ public class GameManager : MonoBehaviour
         Debug.Log("Personajes inelegibles que van al banco: " + ineligibleCharacters.Count);
 
 
-    }
+    }*/
 
-
-    public List<Characters> DeterminateRouteOfCharacters()
-    {
-        //Characters[] charactersInScene = FindObjectsOfType<Characters>();
-
-        List<Characters> ineligibleCharacters = Characters.Aggregate(new List<Characters>(), (list, character) =>
-        {
-            if (character.WantsToAttendConcert && (character.iHaveMoney <= 10 || character.Age < 18))
-            {
-                list.Add(character);
-            }
-            return list;
-        });
-        /*foreach (Characters character in ineligibleCharacters)
-        {
-            character.IdidntGetTheTicketRoute();
-        }
-        */
-        Debug.Log("Personajes inelegibles para asistir al recital: " + ineligibleCharacters.Count);
-        /*
-         int ineligibleCharacters = charactersInScene.Aggregate(0, (count, character) =>
-             (character.WantsToAttendConcert && (character.iHaveMoney <= 10 || character.Age < 18)) ? count + 1 : count);
-
-         Debug.Log("Cantidad de personajes inelegibles para asistir al recital: " + ineligibleCharacters);*/
-        return ineligibleCharacters;
-    }
 
 
 
