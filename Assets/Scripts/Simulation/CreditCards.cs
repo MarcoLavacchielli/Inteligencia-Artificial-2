833966d666557e5061cfd6901e441dad3af7f2b9
Marco <marco.lavacchielli@davinci.edu.ar>
2024-05-16 18:13:37 -0300

    Se cambiaron los nombres en escena

--- Assets/Scripts/Simulation/CreditCards.cs
+++ Assets/Scripts/Simulation/CreditCards.cs
@@ -10,21 +10,3 @@ public class CreditCards : MonoBehaviour
         return Random.Range(0, 10)*multiplier;
     }
 }
-/*
-public class Basic : CreditCards
-{
-    
-    public override int GetCreditLimit()
-    {
-        return Random.Range(1, 5); 
-    }
-}
-public class Premium : CreditCards
-{
-  
-    public override int GetCreditLimit()
-    {
-            return Random.Range(1, 10);
-    }
-}
-*/
